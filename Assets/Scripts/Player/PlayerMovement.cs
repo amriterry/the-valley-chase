ea280353a9f63e8cdbd449045a230b4c728c33bb
Amrit Twanabasu <amrit.cas@gmail.com>
2016-01-16 08:09:46 +0545

    Fixed Sliding Animation for the player and updated some settings.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using App.TheValleyChase.Input.GestureInput;
 using App.TheValleyChase.Input.AccelerometerInput;
 using App.TheValleyChase.Input.GestureInput.Contracts;
@@ -11,39 +10,62 @@ public class PlayerMovement : MonoBehaviour, IOnAccelerometerInput,IOnGestureInp
     public float turnSpeed = 8f;
     public float jumpPower = 6f;
     public float slideTime = 1f;
+    public float slidingHeight = 1f;
     public float groundCheckDistance = 0.3f;
+
 
     private AccelerometerInput accelerometerInput;
     private GestureInput gestureInput;
     private Rigidbody rigidBody;
     private Quaternion targetRotation;
     private Animator animator;
+    private Vector3 originalCapsuleCenter;
+    private CapsuleCollider colider;
+    private Transform playerMesh;
 
     private bool onGround;
     private bool canMove;
     private bool isRotating;
     private bool isSliding;
-    private float speed = 1f;   
-
+    private float speed;
+    private float originalCapsuleHeight;
+    
+    /// <summary>
+    /// Unity's Awake function.
+    /// </summary>
     void Awake() {
         accelerometerInput = GameObject.FindObjectOfType<AccelerometerInput>();
         gestureInput = GameObject.FindObjectOfType<GestureInput>();
 
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
+        colider = GetComponent<CapsuleCollider>();
+        playerMesh = transform.GetChild(1).transform;
     }
     
+    /// <summary>
+    /// Unity's Start function.
+    /// </summary>
 	void Start () {
         accelerometerInput.RegisterListener(this);
         gestureInput.RegisterListener(this);
 
         canMove = true;
+        speed = 1f;
+        originalCapsuleHeight = colider.height;
+        originalCapsuleCenter = colider.center;
 	}
 	
+    /// <summary>
+    /// Unity's Update function.
+    /// </summary>
 	void Update () {
         Move();
 	}
 
+    /// <summary>
+    /// Moves the player in the scene.
+    /// </summary>
     private void Move() {
         CheckGroundStatus();
 
@@ -59,14 +81,28 @@ public class PlayerMovement : MonoBehaviour, IOnAccelerometerInput,IOnGestureInp
         UpdateAnimator();
     }
 
-    private void UpdateSliding() {
-        if (isSliding) {
-            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Sliding") && animator.GetNextAnimatorStateInfo(0).IsName("Grounded")) {
-                isSliding = false;
+    /// <summary>
+    /// Checks if the player is on ground.
+    /// </summary>
+    private void CheckGroundStatus() {
+        RaycastHit hit;
+#if UNITY_EDITOR
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
+#endif
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hit, groundCheckDistance)) {
+            if (hit.collider.tag == TagsContainer.Ground) {
+                onGround = true;
+                animator.applyRootMotion = true;
             }
+        } else {
+            onGround = false;
+            animator.applyRootMotion = false;
         }
     }
-
+    
+    /// <summary>
+    /// Updates Rotation of the player.
+    /// </summary>
     private void UpdateRotation() {
         if (isRotating) {
             Vector3 angle = transform.rotation.eulerAngles - targetRotation.eulerAngles;
@@ -83,6 +119,44 @@ public class PlayerMovement : MonoBehaviour, IOnAccelerometerInput,IOnGestureInp
         }
     }
 
+    /// <summary>
+    /// Updates isSliding variable.
+    /// </summary>
+    private void UpdateSliding() {
+        if (isSliding) {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Sliding") && animator.GetNextAnimatorStateInfo(0).IsName("Grounded")) {
+                isSliding = false;
+            }
+        }
+        ResizeColiderHeight();
+    }
+
+    /// <summary>
+    /// Resizes Colider height based upon the time of animation playback for sliding animation.
+    /// </summary>
+    private void ResizeColiderHeight() {
+        AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
+        if (info.IsName("Sliding")){
+            if(info.normalizedTime >= 0.22) {
+                float timeScale = 8f;
+                if(info.normalizedTime >= 0.5) {
+                    colider.direction = 1;
+                    colider.center = Vector3.Lerp(colider.center, originalCapsuleCenter, Time.deltaTime * timeScale);
+                    playerMesh.localPosition = Vector3.Lerp(playerMesh.localPosition, Vector3.zero, Time.deltaTime * timeScale);
+                    playerMesh.localRotation = Quaternion.Lerp(playerMesh.localRotation,Quaternion.Euler(0, 0, 0),Time.deltaTime * timeScale);
+                } else {
+                    colider.direction = 2;
+                    colider.center = Vector3.Lerp(colider.center,new Vector3(0, 0.25f, 0), Time.deltaTime * timeScale);
+                    playerMesh.localPosition = Vector3.Lerp(playerMesh.localPosition, new Vector3(0, -0.4f, 0), Time.deltaTime * timeScale);
+                    playerMesh.localRotation = Quaternion.Lerp(playerMesh.localRotation,Quaternion.Euler(-25, 0, 0),Time.deltaTime * timeScale);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates animator for various parameters.
+    /// </summary>
     private void UpdateAnimator() {
         animator.SetFloat("Forward", speed);
 
@@ -104,30 +178,26 @@ public class PlayerMovement : MonoBehaviour, IOnAccelerometerInput,IOnGestureInp
         animator.SetBool("OnGround", onGround);
     }
 
-    private void CheckGroundStatus() {
-        RaycastHit hit;
-#if UNITY_EDITOR
-        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
-#endif
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hit, groundCheckDistance)) {
-            if (hit.collider.tag == TagsContainer.Ground) {
-                onGround = true;
-                animator.applyRootMotion = true;
-            }
-        } else {
-            onGround = false;
-            animator.applyRootMotion = false;
-        }
-    }
-
+    /// <summary>
+    /// Called when an accelerometer input is detected.
+    /// </summary>
+    /// <param name="fixedAcceleration"></param>
     public void OnAccelerometerDetected(Vector3 fixedAcceleration) {
         MoveHorizontally(fixedAcceleration.x);
     }
 
+    /// <summary>
+    /// Moves the player horizontally based upon the accelerometer input.
+    /// </summary>
+    /// <param name="x"></param>
     private void MoveHorizontally(float x) {
         transform.position = transform.position + Vector3.Cross(transform.up, transform.forward) * x * accelerometerInput.GetSensitivity();
     }
 
+    /// <summary>
+    /// Called when a gesture input is detected.
+    /// </summary>
+    /// <param name="gesture"></param>
     public void OnSwipe(Gesture gesture) {
         switch (gesture.GetGestureType()) {
             case GestureType.SWIPE_UP:
@@ -145,38 +215,54 @@ public class PlayerMovement : MonoBehaviour, IOnAccelerometerInput,IOnGestureInp
         }
     }
 
+    /// <summary>
+    /// Makes the player jump.
+    /// </summary>
+    private void Jump() {
+        if (onGround && animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded")) {
+            onGround = false;
+            animator.applyRootMotion = false;
+            if (rigidBody.velocity.x == 0f || rigidBody.velocity.z == 0f) {
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpPower, rigidBody.velocity.z) + transform.forward * speed;
+            } else {
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpPower, rigidBody.velocity.z);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Makes the player slide.
+    /// </summary>
     private void Slide() {
         if (onGround && !isSliding) {
             isSliding = true;
         }
     }
 
+    /// <summary>
+    /// Turns the player to right.
+    /// </summary>
     private void TurnRight() {
         ApplyRotation(90);
     }
 
+    /// <summary>
+    /// Turns the player to left.
+    /// </summary>
     private void TurnLeft() {
         ApplyRotation(-90);
     }
 
+    /// <summary>
+    /// Applies rotation to the player.
+    /// </summary>
+    /// <param name="degrees"></param>
     void ApplyRotation(float degrees) {
         if (!isRotating && !isSliding && onGround) {
             Quaternion newRotation = Quaternion.identity;
             newRotation = Quaternion.AngleAxis(degrees, Vector3.up);
             targetRotation = transform.rotation * newRotation;
             isRotating = true;
-        }
-    }
-
-    private void Jump() {
-        if (onGround && animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded")) {
-            onGround = false;
-            animator.applyRootMotion = false;
-            if(rigidBody.velocity.x == 0f || rigidBody.velocity.z == 0f) {
-                rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpPower, rigidBody.velocity.z) + transform.forward * speed;
-            } else {
-                rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpPower, rigidBody.velocity.z);
-            }
         }
     }
 }
